8dc8c0615f5038f60acc5b768873c905a28298ec
chrizxzayax <chmolina629@hotmail.com>
2025-09-28 11:53:15 -0700

    initial

---
+++ lab14.cpp
@@ -0,0 +1,13 @@
+
+// comsc-210 | lab6 | Christian Molina 
+using namespace std;
+
+class Coolor {
+    private:
+        int red;
+        int green;
+        int blue;
+
+
+    public:
+    Coolor() : red(0), green(0), blue(0) {} };
